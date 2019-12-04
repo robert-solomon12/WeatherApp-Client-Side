7b1c9c7bf224189f2c2b06751167a3adf063809c
Robert Solomon <20079462@mail.wit.ie>
2019-12-04 22:24:11 +0000

    CODE UPDATE

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+ class About extends Component {
+
+  render() {
+    return (
+     <div className="about-rs-info">
+   <h1>About RS-Weather</h1>
+   <p>RS-Weather, recognized and documented as the most accurate source of weather forecasts and warnings in the world, has saved tens of thousands of lives, prevented hundreds of thousands of injuries and tens of billions of dollars in property damage. With global headquarters in State College, PA and other offices around the world, AccuWeather serves more than 1.5 billion people daily to help them plan their lives and get more out of their day through radio, television, newspapers, smart phones, tablets, connected TVs, the AccuWeather Network and AccuWeather.com. Additionally, AccuWeather produces and distributes news, weather content, and video for more than 180,000 third-party websites. Among AccuWeather's many innovative and award-winning features available free to the public are MinuteCast® Minute by Minute™ forecasts with Superior Accuracy™. Furthermore, AccuWeather serves 245 of Fortune 500 companies and thousands of businesses globally. Dr. Joel N. Myers, Founder and Chief Executive Officer, established AccuWeather in 1962 and is considered the "father of modern commercial meteorology." Dr. Myers, a leading creative thinker and visionary, has been named "the most accurate man in weather" by The New York Times and one of the top entrepreneurs in American history by Entrepreneur's Encyclopedia of Entrepreneurs.</p>
+     </div>
+    );
+  }
+}
+
+export default About
