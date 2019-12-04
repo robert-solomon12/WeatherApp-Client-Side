b5729a8c0e8ef706e9a3a554d4498fcb5b53e00b
Robert Solomon <20079462@mail.wit.ie>
2019-12-04 22:21:29 +0000

    CODE UPDATE

--- src/App.js
+++ src/App.js
@@ -1,80 +1,59 @@
 import React from 'react';
 import './App.css';
-import Titles from "./components/Titles-Section";
-import Form from "./components/Form-Section";
-import Temperature from "./components/Temperature-Section"
-
-
-
-//My Weather Api generated from openweather.org
-const API_KEY = "a1796d91865e4597b9f8ac69bc32ddc2"
-
+import { Layout, Header, HeaderRow, Textfield, Navigation, Drawer, Footer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+// import Footer from './components/FooterComp';
+// import '../node_modules/font-awesome/css/font-awesome.min.css';
 //All components are rendered in within the div tags below 
-class App extends React.Component {
 
-
-  //Initial State of the fields are left undefined since no info has been requested
-  state = {
-    cityname: undefined,
-    countryname: undefined,
-    temperature: undefined,
-    description: undefined,
-    humidity: undefined,
-    wind_speed: undefined,
-    wind_degree: undefined,
-  }
-  getWeatherInfo = async (e) => {
-
-    //to prevent default behaviour of the component when function is called which causes the page to refresh with no change
-    e.preventDefault();
-    const cityname = e.target.elements.cityname.value;
-    const countryname = e.target.elements.countryname.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityname},${countryname}&appid=${API_KEY}&units=imperial`);
-
-
-    //data object created here to call the api_call object that contains the information from the weather api link
-    const data = await api_call.json();
-    console.log(data)
-
-
-    
-    /*state is being set when button is clicked and a request of information has been
-     triggered by the user's button click and fetched from the API*/
-    this.setState({
-      cityname: data.name,
-      countryname: data.sys.country,
-      temperature: data.main.temp,
-      description: data.weather[0].description,
-      humidity: data.main.humidity,
-      wind_speed: data.wind.speed,
-      wind_degree: data.wind.deg
-    });
-  }
-
-   /* All Components are rendered here to display together in one page of the UI
-   */
-  render() {
+function App() {
     return (
-      <div>
-        <header>
-        <Titles />
-        </header>
-        <Form getWeatherInfo={this.getWeatherInfo}/> 
-        <Temperature
-        cityname = {this.state.cityname }
-        countryname = {this.state.countryname }
+        <div style={{height: 'auto', width: 'auto', margin: 'auto'}}className="demo-big-content">
+            <Layout>
+                <Header className="header-design" title="Title" scroll>
+                    <HeaderRow title="Robert's Weather Application">
+                        <Textfield
+                            value=""
+                            onChange={() => { }}
+                            label="Search2"
+                            expandable
+                            expandableIcon="search"
+                        />
+                    </HeaderRow>
+                    <HeaderRow>
+                        <Navigation>
+                            <Link to="/Home">Home</Link>
+                            <Link to="/weatherView">Weather View</Link>
+                            <Link to="/Contact">Contact</Link>
+                            <Link to="/About">About Weather App</Link>
+                        </Navigation>
+                    </HeaderRow>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                        <Link to="/Home">Home</Link>
+                        <Link to="/weatherView">Weather View</Link>
+                        <Link to="/Contact">Contact</Link>
+                        <Link to="/About">About WeatherApp</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main />
 
-        temperature = {this.state.temperature }
-        description = {this.state.description }
-        humidity = { this.state.humidity }
-        wind_speed = { this.state.wind_speed }
-        wind_degree = { this.state.wind_degree }
-        />
-
+                    <Footer className="footer-design" title="Title" scroll>
+                   <p> And this right here is a spiffy footer, where you can put stuff.</p>
+        <a className="icon" href="#"><i className="fa fa-facebook"></i></a>
+        <a className="icon" href="#"><i className="fa fa-twitter"></i></a>
+                </Footer>
+                </Content>    
+            </Layout>
         </div>
     );
-  }
-};
-  
+}
 
 export default App;
+
+
+ 
